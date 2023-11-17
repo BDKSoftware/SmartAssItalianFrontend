52ec2ef05e4991ceba43324f9e135c8342eebdd6
BDKSoftware <kookcodes@gmail.com>
2023-11-17 11:16:44 -0700

    First

---
+++ src/scenes/global/Footer.jsx
@@ -0,0 +1,50 @@
+import { useTheme } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { shades } from "../../theme";
+import logo from "../../logo/logo.jpg";
+
+const Footer = () => {
+  const {
+    palette: { neutral },
+  } = useTheme();
+
+  return (
+    <Box
+      mt="70px"
+      p="40px 0"
+      display="absolute"
+      bottom="0"
+      backgroundColor="black"
+    >
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        flexWrap="wrap"
+        rowGap="30px"
+        columnGap="clamp(20px, 30px, 40px)"
+      >
+        <Box width="clamp(20%, 30%, 40%)" display="flex" alignItems="center">
+          <img alt="logo" src={logo} width={300} height={200} />
+        </Box>
+        <Box width="clamp(20%, 25%, 30%)">
+          <Typography variant="h4" fontWeight="bold" mb="30px" color={"white"}>
+            Contact Us
+          </Typography>
+          <Typography mb="30px" color={"white"}>
+            100 Fake Street, Scottsdale, AZ
+          </Typography>
+          <Typography mb="30px" sx={{ wordWrap: "break-word" }} color={"white"}>
+            Email: Johntaglia@gmail.com
+          </Typography>
+          <Typography mb="30px" color={"white"}>
+            Phone: (602)-390-6063
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
---
+++ src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,99 @@
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { shades } from "../../theme";
+
+const importAll = (r) => {
+  return r.keys().reduce((acc, item) => {
+    acc[item.replace("./", "")] = r(item);
+    return acc;
+  }, {});
+};
+
+export const heroTextureImports = importAll(
+  require.context("../../assets/", false, /\.(png|jpe?g|svg)$/)
+);
+
+const MainCarousel = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+
+  return (
+    <div style={{ width: "100%", backgroundColor: shades.neutral[500] }}>
+      <Carousel
+        style={{ backgroundColor: shades.secondary[500] }}
+        infiniteLoop={true}
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        renderArrowPrev={(onClickHandler, hasPrev, label) => (
+          <IconButton
+            onClick={onClickHandler}
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "0",
+              color: shades.secondary[500],
+              padding: "5px",
+              zindex: "10",
+            }}
+          >
+            <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+          </IconButton>
+        )}
+        renderArrowNext={(onClickHandler, hasNext, label) => (
+          <IconButton
+            onClick={onClickHandler}
+            sx={{
+              position: "absolute",
+              top: "50%",
+              right: "0",
+              color: shades.secondary[500],
+              padding: "5px",
+              zindex: "10",
+            }}
+          >
+            <NavigateNextIcon sx={{ fontSize: 40 }} />
+          </IconButton>
+        )}
+      >
+        {Object.values(heroTextureImports).map((texture, index) => (
+          <Box key={`carousel-image-${index}`}>
+            <img
+              src={texture}
+              alt={`carousel-${index}`}
+              style={{
+                width: "100%",
+                height: "700px",
+                objectFit: "contain",
+                backgroundAttachment: "fixed",
+              }}
+            />
+
+            <Box
+              color="white"
+              padding="20px"
+              borderRadius="1px"
+              textAlign="left"
+              backgroundColor="rgba(0,0,0,0.4)"
+              position="absolute"
+              top="46%"
+              left={isNonMobile ? "10%" : "0"}
+              right={isNonMobile ? undefined : "0"}
+              margin={isNonMobile ? undefined : "0 auto"}
+              maxWidth={isNonMobile ? undefined : "240px"}
+            >
+              <Typography variant="h1">Christmas Sale!</Typography>
+              <Typography fontWeight="500" color={shades.neutral[200]}>
+                New Items Released
+              </Typography>
+            </Box>
+          </Box>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default MainCarousel;
